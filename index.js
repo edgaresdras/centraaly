b94c32357d43f21c6d3eafda59ac59365aecc079
esdrasgreco <esdras_greco@yahoo.com>
2019-03-14 20:18:24 -0600

    get method for / responde a welcome message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const port = 8080;
+const chalk = require('chalk'); //Pintar mensajes en la consola
+ 
+//Escuchar peticiones en un puerto
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+    console.log(chalk.cyan(`http://localhost:${port}`));
+});
+
+//Rutas
+app.get('/', (req, res) => {
+    res.send('Estoy respondiendo algo');
+});
